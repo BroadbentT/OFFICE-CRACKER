3d0b80b4cc58cfb7c220a126f4b0e2ee74df6693
root <root@kali.Domain>
2021-03-24 18:38:37 +0000

    Python3

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # MICROSOFT OFFICE CRACKER
 ## A PYTHON SCRIPT FILE TO CRACK ENCRYPTED MICROSOFT OFFICE FILES.
 
-Usage: python office-cracker.py topsecret.docx
+Usage: python3 office-cracker.py topsecret.docx
 
 | LANGUAGE | FILENAME          | MD5 HASH                         |
 |--------  |---------          |---------                         |
-| python   | office-cracker.py | 69668c24c07a7c32d2091a16c14c9032 |
+| python3  | office-cracker.py | 84459ebe7ff85c3c667069e80ccce350 |
 | doc      | topsecret.doc     | deabfc23261259cec48f226ef154df51 |
 | xlsx     | topsecret.xlsx    | 8c977a4286601341728f121fc9a02752 |
 
 A python script file to crack encrypted Microsoft Office files using bruteforce. The script is menu driven and allows the user to choose between a dictionary attack or a hash attack on the specified file.
 
 ### CONSOLE DISPLAY
-![Screenshot](picture1.png)
+![Screenshot](picture1.gif)
+
+### COMPUTER MISUSE ACT 1990 - SECTION 3A
+This software program has been specifically written for the purpose of legitimate penetration testing and should not be used for any other unauthorised or nefarious reasons.
+
+Found this project useful, or would like to buy the developer a coffee - make a donation.
+https://paypal.me/TerenceBroadbent
--- office-cracker.py
+++ office-cracker.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # coding:UTF-8
 
 # -------------------------------------------------------------------------------------
@@ -9,99 +9,107 @@
 # ------------------------------------------------------------------------------------- 
 # AUTHOR  : Terence Broadbent                                                    
 # CONTRACT: GitHub                                                               
-# Version : 2.0                                                                
+# Version : 3.0                                                               
 # Details : Load required imports.
 # Modified: N/A
 # -------------------------------------------------------------------------------------
 
 import os
 import sys
+import pyfiglet
 import fileinput
+
+from termcolor import colored
 
 # -------------------------------------------------------------------------------------
 # AUTHOR  : Terence Broadbent                                                    
 # CONTRACT: GitHub                                                               
-# Version : 2.0                                                                
+# Version : 3.0                                                               
 # Details : Conduct simple and routine tests on supplied arguements.   
 # Modified: N/A                                                               
 # -------------------------------------------------------------------------------------
 
 if os.geteuid() != 0:
-    print "Please run this python script as root..."
-    exit(True)
+   print("Please run this python script as root...")
+   exit()
 
 if len(sys.argv) < 2:
-    print "Use the command python office-cracker.py microsoft.docx..."
-    exit(True)
+   print("Use the command python3 office-cracker.py topsecret.docx...")
+   exit()
 
-filename = sys.argv[1]
+fileName = sys.argv[1]
 
-if os.path.exists(filename) == 0:
-    print "File " + filename + " was not found, did you spell it correctly?..."
-    exit(True)
+if os.path.exists(fileName) == 0:
+   print("File " + fileName + " was not found, did you spell it correctly?...")
+   exit()
 
-# -------------------------------------------------------------------------------------
-# AUTHOR  : Terence Broadbent                                                    
-# CONTRACT: GitHub                                                               
-# Version : 2.0                                                                
-# Details : Check that the specified file is a Microsoft Office file.
-# Modified: N/A                                                               
-# -------------------------------------------------------------------------------------
-
-filextends = filename[-4:]
+filextends = fileName[-4:]
 officelist = ["docx", ".doc", ".xls", "xlsx", ".ppt", "ccdb", ".pub"]
 
 if filextends not in officelist:
-    print "Unknown Microsoft Office file format...."
-    exit (True)
+   print("This is not a Microsoft Office file...\n")
+   exit()
 
 # -------------------------------------------------------------------------------------
 # AUTHOR  : Terence Broadbent                                                    
 # CONTRACT: GitHub                                                               
-# Version : 2.0                                                                
-# Details : Create function to display my universal banner.
+# Version : 3.0                                                               
+# Details : Create a function to display my universal banner.
 # Modified: N/A                                                               
 # -------------------------------------------------------------------------------------
 
-def display(filename):
+def display(fileName):
    os.system("clear")
-   print "  ___  _____ _____ ___ ____ _____       ____ ____      _    ____ _  _______ ____   "
-   print " / _ \|  ___|  ___|_ _/ ___| ____|     / ___|  _ \    / \  / ___| |/ / ____|  _ \  "
-   print "| | | | |_  | |_   | | |   |  _| _____| |   | |_) |  / _ \| |   | ' /|  _| | |_) | "
-   print "| |_| |  _| |  _|  | | |___| |__|_____| |___|  _ <  / ___ \ |___| . \| |___|  _ <  "
-   print " \___/|_|   |_|   |___\____|_____|     \____|_| \_\/_/   \_\____|_|\_\_____|_| \_\ "
-   print "                                                                                   "
-   print "             BY TERENCE BROADBENT BSC CYBER SECURITY (FIRST CLASS)                 "
-   print "                                                                                   "
-   print "FILENAME: " + filename + ".\n"
+   ascii_banner = pyfiglet.figlet_format("OFFICE CRACKER").upper()
+   print((colored(ascii_banner.rstrip("\n"), 'red', attrs=['bold'])))
+   print((colored("     BY TERENCE BROADBENT BSC CYBER SECURITY (FIRST CLASS)     \n", 'yellow', attrs=['bold'])))
+   print("Selected fileName : " + fileName + "\n")
 
 # -------------------------------------------------------------------------------------
 # AUTHOR  : Terence Broadbent                                                    
 # CONTRACT: GitHub                                                               
-# Version : 2.0                                                                
+# Version : 3.0                                                               
 # Details : Check all required dependencies are installed on the system.
 # Modified: N/A                                                               
 # -------------------------------------------------------------------------------------
 
-checklist = ["rockyou", "office2john", "hashcat"]
+checklist = ["/usr/bin/hashcat"]
 installed = True
 
-display(filename)
+display(fileName)
 for check in checklist:
-    cmd = "locate -i " + check + " > /dev/null"
-    checked = os.system(cmd)
-    if checked != 0:
-        print "I could not find " + check + "..."
-        installed = False
+   cmd = "locate -i " + check + " > /dev/null"
+   checked = os.system(cmd)
+   if checked != 0:
+      print("I could not find " + check + "...")
+      installed = False
 
 if installed == False:
-   print "\nInstall missing dependencies before you begin...\n"
-   exit (True)
+   print("\nInstall the above missing dependencies before you begin...\n")
+   exit()  
+   
+# -------------------------------------------------------------------------------------
+# AUTHOR  : Terence Broadbent                                                    
+# CONTRACT: GitHub                                                               
+# Version : 3.0                                                               
+# Details : User changeable dependencies.
+# Modified: N/A                                                               
+# -------------------------------------------------------------------------------------
+   
+hashcrack = "/usr/share/john/office2john.py"									# USER CHANGEABLE LOCATION OF OFFICETOJOHN
+if not os.path.exists(hashcrack):
+  print("The identified file on line 99 of this script, was not found!!...\n")
+  exit()   
+  
+dictionary = "/usr/share/wordlists/rockyou.txt"									# USER CHANGEABLE LOCATION OF DICTIONARY
+if not os.path.exists(dictionary):
+   print("The identified dictionary on line 104 of this script, was not found!!...\n")
+   exit()
 
 # -------------------------------------------------------------------------------------
 # AUTHOR  : Terence Broadbent                                                    
 # CONTRACT: GitHub                                                               
-# Version : 2.0                                                                
+# Version : 3.0                                                               
 # Details : The main menu system.
 # Modified: N/A                                                               
 # -------------------------------------------------------------------------------------
@@ -112,44 +120,36 @@ menu['2']="Hash Attack."
 menu['3']="Exit"
 
 while True: 
-    display(filename)
-    options=menu.keys()
-    options.sort()
-    for entry in options: 
-        print entry, menu[entry]
-    selection=raw_input("Please Select: ")
+   display(fileName)
+   options=list(menu.keys())
+   options.sort()
+   for entry in options: 
+      print(entry, menu[entry])
+   print(colored("\n[?] Please select an option: ",'green'),end='')
+   selection=input()
 
 # ------------------------------------------------------------------------------------- 
 # AUTHOR  : Terence Broadbent                                                    
 # CONTRACT: GitHub                                                               
-# Version : 2.0                                                                
+# Version : 3.0                                                               
 # Details : Menu option selected - Dictionary attack.
 # Modified: N/A
 # -------------------------------------------------------------------------------------
 
-    if selection =='1':
-        print "\nCrack Selected    : Dictionary attack..."
-        dictionary = "/usr/share/wordlists/rockyou.txt"
-        if os.path.isfile(dictionary):
-            print "Using Dictionary  : " + dictionary + "..."
-        else:
-            print "System Error      : Dictionary not found..."
-            exit(True)
-        os.system("office2john.py '" + filename + "' > Hash.txt")
-        os.system("john --encoding=UTF-8 --wordlist=" + dictionary + " Hash.txt > Temp.txt 2>&1")
-        os.system("john --show Hash.txt > Answer.txt")
-	os.system("sed '$d' Answer.txt > Password.txt")
-	with open('Password.txt', 'r') as myfile:
-            password = myfile.read().replace(filename + ":", '')
-        if password == "Could not find password":
-            print "Crack Status      : Dictionary exhausted..."
-        else:      
-            print "File Password     : '" + password.rstrip() + "'\n"
-        os.remove('Hash.txt')
-        os.remove('Answer.txt')
-        os.remove('Password.txt')
-        os.remove('Temp.txt')
-        exit (False)
+   if selection =='1':
+      print("\n[+] Crack Status : Using dictionary " + dictionary + "...")             
+      print("[+] Crack Status : Using words in dictionary as password, please wait...")   
+      os.system("python2.7 " + hashcrack + " '" + fileName + "' > F1.tmp")					# NOTE PYTHON 2.7 REQUIRED HERE!!
+      os.system("john --encoding=UTF-8 --wordlist=" + dictionary + " F1.tmp > F2.tmp 2>&1")
+      os.system("john --show F1.tmp > F2.tmp")
+      os.system("sed -i '$d' F2.tmp")      
+      password = open("F2.tmp").readline().replace(fileName + ":","")
+      if password == "Could not find password":
+         print("[-] Crack Status      : Dictionary exhausted...")
+      else:      
+         print(colored("\n[!] Found password '" + password.rstrip("\n") + "'\n",'green'))         
+      os.system("rm *.tmp")        
+      exit()
 
 # ------------------------------------------------------------------------------------- 
 # AUTHOR: Terence Broadbent                                                    
@@ -159,66 +159,60 @@ while True:
 # Modified: N/A
 # -------------------------------------------------------------------------------------
 
-    elif selection == '2':
-        print "\nCrack Selected    : Hash attack..."
-        os.system("office2john.py '" + filename + "' > Hash.txt")
-	with open('Hash.txt', 'r') as myfile:
-            hashdata = myfile.read().replace(filename + ":", '')
-	for line in fileinput.input('Hash.txt', inplace=1):
-            sys.stdout.write(line.replace(filename + ":", ''))
-        hashdata = hashdata[:55]    
-        print "Hash Extracted    : " + hashdata + "..."
-	hashdata = hashdata[:13]    
-        hashdata = hashdata[-4:]
-        print "MS Office Version : " + hashdata
-        if hashdata == "2013":
-            level = "9600"
-        elif hashdata == "2010":
-            level = "9500"
-        elif hashdata == "2007":
-            level = "9400"
-	elif hashdata == "2003":
-            level = "9700"
-        else:
-            print "Crack Status      :Unknown year..."
-            os.remove("Hash.txt")
-            exit (True)
-        print "Using Hashcat Mode: " + level
-        os.system("hashcat -m " + level + " -a 3 Hash.txt -i ?d?d?d?d?d?d --force > Temp.txt 2>&1")
-        os.system("hashcat --show -m " + level + " Hash.txt --force > Answer.txt")
-        os.system("awk -F: '{ print $2 }' Answer.txt > Password.txt")
-        with open('Password.txt', 'r') as myfile:
-            password = myfile.read().replace('\n', '')
-        if password == "":
-            print "Crack Status      : Algorithm exhausted...\n"
-        else:
-            print "Found Password    : '" + password + "'\n"
-        os.remove('Hash.txt')
-        os.remove('Answer.txt')
-        os.remove('Password.txt')
-        os.remove('Temp.txt')
-        exit (True)
+   if selection == '2':
+      os.system("python2.7 " + hashcrack + " '" + fileName + "' > F1.tmp")
+      hashdata = open("F1.tmp").readline().replace(fileName + ":","")
+      os.system("sed -i 's/" + fileName + "://g' F1.tmp")
+      print("\nHash Extracted    : " + hashdata[:55] + "...")
+      hashdata = hashdata[:13]
+      hashdata = hashdata[-4:]
+      print("MS Office Version : " + hashdata)
+      if hashdata == "2013":
+         level = "9600"
+      elif hashdata == "2010":
+         level = "9500"
+      elif hashdata == "2007":
+          level = "9400"
+      elif hashdata == "2003":
+         level = "9700"
+      else:
+         print("\n[-] Crack Status      : Unknown year...")
+         os.system("rm *.tmp")
+         exit()
+      print("Using Hashcat Mode: " + level)
+      print("Crack Status      : Cracking hash values, please wait...")
+      os.system("hashcat -m " + level + " -a 3 F1.tmp -i ?d?d?d?d?d?d --force > F2.tmp 2>&1")
+      os.system("hashcat --show -m " + level + " F1.tmp --force > F3.tmp")
+      os.system("awk -F: '{ print $2 }' F3.tmp > F4.tmp")
+      password = open("F4.tmp").readline().replace("\n","")
+      if password == "":
+         print("Crack Status      : Hash values exhausted...\n")
+      else:
+         print(colored("\n[!] Found password '" + password.rstrip("\n") + "'\n",'green'))
+      os.system("rm *.tmp")
+      exit()
  
 # ------------------------------------------------------------------------------------- 
 # AUTHOR  : Terence Broadbent                                                    
 # CONTRACT: GitHub                                                               
-# Version : 2.0                                                                
+# Version : 3.0                                                               
 # Details : Menu option selected - Quit program.
 # Modified: N/A
 # -------------------------------------------------------------------------------------
 
-    elif selection == '3': 
-        break
+   if selection == '3': 
+      print("\n")
+      quit()
 
 # ------------------------------------------------------------------------------------- 
 # AUTHOR  : Terence Broadbent                                                    
 # CONTRACT: GitHub                                                               
-# Version : 2.0                                                                
+# Version : 3.0                                                               
 # Details : Catch all other entries.
 # Modified: N/A
 # -------------------------------------------------------------------------------------
 
-    else:
-        print ""
+   else:
+      pass
 
 #Eof
